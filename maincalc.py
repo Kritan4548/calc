db7814568bbb31aa7cc6d8894e5c0fedf0e3454d
Kritan4548 <kritan.sitaula2021@gmail.com>
2021-04-01 19:31:31 +0545

    Main file done

---
+++ maincalc.py
@@ -0,0 +1,16 @@
+#This is by kritan sitaula
+import add_work
+import div_work
+import mul_work
+import sub_work
+
+a=110
+b=50
+
+total_sum=add_work.add(a,b)
+total_sub=sub_work.sub(a,b)
+total_div=div_work.sub(a,b)
+total_mul=mul_work.sub(a,b)
+
+print(f"Total sum is {total_sum}. Total sub is {total_sub}. Total mul is {total_mul}. Total div is {total_div}."
+
